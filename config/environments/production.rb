f33bc1e8c29f262cbf198d873db491ccfb501928
micmac <mike@mikebarb.net>
2018-02-23 05:14:45 +0000

    update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,16 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # devise action mailer configuration 
+  # - required to properly generate links inside the e-mail views
+  config.action_mailer.default_url_options = { :host => ENV['DEFAULT_HOST'] }
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.mailgun_settings = {
+    :api_key              => ENV['MAILGUN_API_KEY'],
+    :domain               => ENV['MAILGUN_DOMAIN']
+  }
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
