e17d20d2a07f917acc43072741bb2c520d563e23
micmac <mike@mikebarb.net>
2018-05-24 00:00:50 +0000

    added fix to uglifier config in prod to enable uploading to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,11 @@ Rails.application.configure do
   config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # this failed loading heroku - implemented comemnts from:
+  # https://github.com/lautis/uglifier/issues/127
+  #config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = :uglifier(harmony: true)
+
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
