0d6c800d0e815a38e42d1e7ac55ecde925c791bd
micmac <mike@mikebarb.net>
2018-12-31 05:06:28 +0000

    initial api deployment

--- db/migrate/20181221031557_add_authentication_token_to_users.rb
+++ db/migrate/20181221031557_add_authentication_token_to_users.rb
@@ -2,5 +2,10 @@ class AddAuthenticationTokenToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :auth_token, :string
     add_index :users, :auth_token, unique: true
+    # Add an initial auth_token to existing users
+    User.all.each do |this|
+      this.auth_token = Devise.friendly_token
+      this.save!
+    end
   end
 end
